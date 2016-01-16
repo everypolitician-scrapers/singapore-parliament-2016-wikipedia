edf928f92ce46fb5ae552937eea8e92a13fa17cc
Tony Bowden <tony@tmtm.com>
2016-01-16 09:34:55 +0000

    Add 2006 members

--- scraper.rb
+++ scraper.rb
@@ -4,11 +4,19 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names_11 = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/Template:Singapore_Parliament_2006',
+  xpath: '//table[contains(@class,"navbox-inner")]//td[contains(@class,"navbox-list")]//ul//li//a[not(@class="new")]/@title',
+).reject { |n| n.end_with? 'Constituency' }
+
+names_13 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/Template:Singapore_Parliament_2016',
   xpath: '//table[contains(@class,"navbox-inner")]//td[contains(@class,"navbox-list")]//ul//li//a[not(@class="new")]/@title',
-  debug: true,
 ).reject { |n| n.end_with? 'Constituency' }
+
+names = (names_11 + names_13).uniq
+binding.pry
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
 warn EveryPolitician::Wikidata.notify_rebuilder
 
