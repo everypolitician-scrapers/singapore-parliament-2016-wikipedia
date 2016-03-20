ca4255e1bf40bb6ddf6841954c7b98c689b9e06b
Tony Bowden <tony@tmtm.com>
2016-03-20 09:33:59 +0000

    Turn off debugging

--- scraper.rb
+++ scraper.rb
@@ -12,7 +12,6 @@ names_11 = EveryPolitician::Wikidata.wikipedia_xpath(
 names_12 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/Template:Singapore_Parliament_2011',
   xpath: '//table[contains(@class,"navbox-inner")]//td[contains(@class,"navbox-list")]//ul//li//a[not(@class="new")]/@title',
-  debug: true
 ).reject { |n| n.end_with? 'Constituency' }
 
 names_13 = EveryPolitician::Wikidata.wikipedia_xpath( 
