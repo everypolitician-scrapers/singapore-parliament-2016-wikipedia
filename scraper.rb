bbb364d94036b872f51e204f615f4309dc23ee62
Tony Bowden <tony@tmtm.com>
2016-03-20 09:30:19 +0000

    More consistent naming

--- scraper.rb
+++ scraper.rb
@@ -4,12 +4,12 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-names_06 = EveryPolitician::Wikidata.wikipedia_xpath( 
+names_11 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/Template:Singapore_Parliament_2006',
   xpath: '//table[contains(@class,"navbox-inner")]//td[contains(@class,"navbox-list")]//ul//li//a[not(@class="new")]/@title',
 ).reject { |n| n.end_with? 'Constituency' }
 
-names_11 = EveryPolitician::Wikidata.wikipedia_xpath( 
+names_12 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/Template:Singapore_Parliament_2011',
   xpath: '//table[contains(@class,"navbox-inner")]//td[contains(@class,"navbox-list")]//ul//li//a[not(@class="new")]/@title',
   debug: true
@@ -20,6 +20,6 @@ names_13 = EveryPolitician::Wikidata.wikipedia_xpath(
   xpath: '//table[contains(@class,"navbox-inner")]//td[contains(@class,"navbox-list")]//ul//li//a[not(@class="new")]/@title',
 ).reject { |n| n.end_with? 'Constituency' }
 
-names = (names_06 + names_11 + names_13).uniq
+names = (names_11 + names_12 + names_13).uniq
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
